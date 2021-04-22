5e6bf7ac67d619002812596d90958190bb3d74f8
Rochan2001 <bahadur.rochan@gmail.com>
2021-04-22 10:02:32 +0530

    changes

--- output_images/test.py
+++ output_images/test.py
@@ -2,8 +2,10 @@ import cv2
 import glob
 import numpy as np
 
-for i in glob.glob("/home/rochansingh/Documents/Project/output_images/test_files/*/*.jpeg"):
-  mas=cv2.imread(i)
-  mas=mas[:,:,2]
-  mas=np.where(mas!=0,255,0)
-  cv2.imwrite(i,mas)
+for i in glob.glob(
+    "/home/rochansingh/Documents/Project/output_images/mydz_mask_files/*/*.jpeg"
+):
+    mas = cv2.imread(i)
+    mas = mas[:, :, 2]
+    mas = np.where(mas != 0, 255, 0)
+    cv2.imwrite(i, mas)
