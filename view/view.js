64259527fa1b7fc11241fa1ef7209c645d5411a0
B.ROCHAN SINGH <bahadur.rochan@gmail.com>
2021-04-27 15:17:15 +0530

    synced mask and normal image

--- view/view.js
+++ view/view.js
@@ -1,16 +1,50 @@
-var viewer = OpenSeadragon({
-  id: "openseadragon",
+var viewer1 = OpenSeadragon({
+  id: "openseadragon1",
   prefixUrl: "../openseadragon/images/",
   tileSources: "../output_images/mydz.dzi",
   visibilityRatio: 1.0,
   constrainDuringPan: true,
 });
 
-var anno = OpenSeadragon.Annotorious(viewer);
+var viewer2 = OpenSeadragon({
+  id: "openseadragon2",
+  prefixUrl: "../openseadragon/images/",
+  tileSources: "../output_images/mydz_mask.dzi",
+  visibilityRatio: 1.0,
+  constrainDuringPan: true,
+});
 
-Annotorious.Toolbar(anno, document.getElementById("toolbar-container"));
+var anno1 = OpenSeadragon.Annotorious(viewer2);
+var anno2 = OpenSeadragon.Annotorious(viewer1);
 
-anno.addDrawingTool(plugin);
+Annotorious.Toolbar(anno1, document.getElementById("toolbar-container2"));
+Annotorious.Toolbar(anno2, document.getElementById("toolbar-container1"));
+
+viewer1.addHandler("canvas-drag", function () {
+  const center1 = viewer1.viewport.getCenter();
+  viewer2.viewport.panTo(center1);
+  console.log(center1);
+});
+
+viewer2.addHandler("canvas-drag", function () {
+  const center2 = viewer2.viewport.getCenter();
+  viewer1.viewport.panTo(center2);
+  console.log(center2);
+});
+
+viewer1.addHandler("canvas-scroll", function () {
+  const zoom1 = viewer1.viewport.getZoom();
+  const center1 = viewer1.viewport.getCenter();
+  viewer2.viewport.panTo(center1);
+  viewer2.viewport.zoomTo(zoom1);
+});
+
+viewer2.addHandler("canvas-scroll", function () {
+  const zoom2 = viewer2.viewport.getZoom();
+  const center2 = viewer2.viewport.getCenter();
+  viewer1.viewport.panTo(center2);
+  viewer1.viewport.zoomTo(zoom2);
+});
 
 // // Load annotations in W3C WebAnnotation format
 // anno.loadAnnotations("annotations.w3c.json");
